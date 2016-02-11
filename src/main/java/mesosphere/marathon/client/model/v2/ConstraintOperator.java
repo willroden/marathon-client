c10cb906266c30b2bfc47bd6816ac8a8fac33c0d
Steven E. Harris <seharris@us.ibm.com>
2016-02-11 17:01:26 -0500

    Add missing ConstraintOperator enum elements
    The Marathon constraint operator set has grown to include the "LIKE" and
    "UNLIKE" operators.

--- src/main/java/mesosphere/marathon/client/model/v2/ConstraintOperator.java
+++ src/main/java/mesosphere/marathon/client/model/v2/ConstraintOperator.java
@@ -1,5 +1,13 @@
 package mesosphere.marathon.client.model.v2;
 
 public enum ConstraintOperator {
-	UNIQUE, CLUSTER, GROUP_BY;
+	// Per the Constraint.Operator enum defined in file
+	//   marathon/src/main/proto/marathon.proto
+	// and the JSON schema defined in file
+	//   marathon/docs/docs/rest-api/public/api/v2/schema/AppDefinition.json:
+	UNIQUE,
+	CLUSTER,
+	GROUP_BY,
+	LIKE,
+	UNLIKE;
 }
