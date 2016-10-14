3a19aa5d7d11e13e4e66b65aae4481f7674665ab
jestum <jesse.estum@optum.com>
2016-10-14 09:52:36 -0500

    Fixing whitespace to declutter diffs

--- src/main/java/mesosphere/marathon/client/model/v2/App.java
+++ src/main/java/mesosphere/marathon/client/model/v2/App.java
@@ -9,26 +9,26 @@ import java.util.Map;
 import mesosphere.marathon.client.utils.ModelUtils;
 
 public class App {
-    private String id;
-    private String cmd;
-    private String version;
-    private Integer instances;
-    private Double cpus;
-    private Double mem;
-    private Collection<String> uris;
-    private List<List<String>> constraints;
-    private Container container;
-    private Map<String, String> env;
-    private Map<String, String> labels;
-    private String executor;
-    private List<Integer> ports;
-    private Collection<Task> tasks;
-    private Integer tasksStaged;
-    private Integer tasksRunning;
+	private String id;
+	private String cmd;
+	private String version;
+	private Integer instances;
+	private Double cpus;
+	private Double mem;
+	private Collection<String> uris;
+	private List<List<String>> constraints;
+	private Container container;
+	private Map<String, String> env;
+	private Map<String, String> labels;
+	private String executor;
+	private List<Integer> ports;
+	private Collection<Task> tasks;
+	private Integer tasksStaged;
+	private Integer tasksRunning;
     private Collection<HealthCheck> healthChecks;
     private UpgradeStrategy upgradeStrategy;
     private Collection<Deployment> deployments;
-    private List<String> dependencies;
+	private List<String> dependencies;
 
     public UpgradeStrategy getUpgradeStrategy() {
         return upgradeStrategy;
@@ -39,149 +39,149 @@ public class App {
     }
 
     public String getId() {
-        return id;
-    }
+		return id;
+	}
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public String getCmd() {
-        return cmd;
-    }
+	public String getCmd() {
+		return cmd;
+	}
 
-    public void setCmd(String cmd) {
-        this.cmd = cmd;
-    }
+	public void setCmd(String cmd) {
+		this.cmd = cmd;
+	}
 
-    public String getVersion() {
-        return version;
-    }
+	public String getVersion() {
+		return version;
+	}
 
-    public void setVersion(String version) {
-        this.version = version;
-    }
+	public void setVersion(String version) {
+		this.version = version;
+	}
 
-    public Integer getInstances() {
-        return instances;
-    }
+	public Integer getInstances() {
+		return instances;
+	}
 
-    public void setInstances(Integer instances) {
-        this.instances = instances;
-    }
+	public void setInstances(Integer instances) {
+		this.instances = instances;
+	}
 
-    public Double getCpus() {
-        return cpus;
-    }
+	public Double getCpus() {
+		return cpus;
+	}
 
-    public void setCpus(Double cpus) {
-        this.cpus = cpus;
-    }
+	public void setCpus(Double cpus) {
+		this.cpus = cpus;
+	}
 
-    public Double getMem() {
-        return mem;
-    }
+	public Double getMem() {
+		return mem;
+	}
 
-    public void setMem(Double mem) {
-        this.mem = mem;
-    }
+	public void setMem(Double mem) {
+		this.mem = mem;
+	}
 
-    public Collection<String> getUris() {
-        return uris;
-    }
+	public Collection<String> getUris() {
+		return uris;
+	}
 
-    public void setUris(Collection<String> uris) {
-        this.uris = uris;
-    }
+	public void setUris(Collection<String> uris) {
+		this.uris = uris;
+	}
 
-    public List<List<String>> getConstraints() {
-        return constraints;
-    }
+	public List<List<String>> getConstraints() {
+		return constraints;
+	}
 
-    public void setConstraints(List<List<String>> constraints) {
-        this.constraints = constraints;
-    }
+	public void setConstraints(List<List<String>> constraints) {
+		this.constraints = constraints;
+	}
 
-    public void addConstraint(String attribute, String operator, String value) {
-        if (this.constraints == null) {
-            this.constraints = new ArrayList<List<String>>();
-        }
-        List<String> constraint = new ArrayList<String>(3);
-        constraint.add(attribute == null ? "" : attribute);
-        constraint.add(operator == null ? "" : operator);
-        constraint.add(value == null ? "" : value);
-        this.constraints.add(constraint);
-    }
+	public void addConstraint(String attribute, String operator, String value) {
+		if (this.constraints == null) {
+			this.constraints = new ArrayList<List<String>>();
+		}
+		List<String> constraint = new ArrayList<String>(3);
+		constraint.add(attribute == null ? "" : attribute);
+		constraint.add(operator == null ? "" : operator);
+		constraint.add(value == null ? "" : value);
+		this.constraints.add(constraint);
+	}
 
-    public Container getContainer() {
-        return container;
-    }
+	public Container getContainer() {
+		return container;
+	}
 
-    public void setContainer(Container container) {
-        this.container = container;
-    }
+	public void setContainer(Container container) {
+		this.container = container;
+	}
 
-    public Map<String, String> getEnv() {
-        return env;
-    }
+	public Map<String, String> getEnv() {
+		return env;
+	}
 
-    public void setEnv(Map<String, String> env) {
-        this.env = env;
-    }
+	public void setEnv(Map<String, String> env) {
+		this.env = env;
+	}
 
-    public String getExecutor() {
-        return executor;
-    }
+	public String getExecutor() {
+		return executor;
+	}
 
-    public void setExecutor(String executor) {
-        this.executor = executor;
-    }
+	public void setExecutor(String executor) {
+		this.executor = executor;
+	}
 
-    public List<Integer> getPorts() {
-        return ports;
-    }
+	public List<Integer> getPorts() {
+		return ports;
+	}
 
-    public void setPorts(List<Integer> ports) {
-        this.ports = ports;
-    }
+	public void setPorts(List<Integer> ports) {
+		this.ports = ports;
+	}
 
-    public void addUri(String uri) {
-        if (this.uris == null) {
-            this.uris = new ArrayList<String>();
-        }
-        this.uris.add(uri);
-    }
+	public void addUri(String uri) {
+		if (this.uris == null) {
+			this.uris = new ArrayList<String>();
+		}
+		this.uris.add(uri);
+	}
 
-    public void addPort(int port) {
-        if (this.ports == null) {
-            this.ports = new ArrayList<Integer>();
-        }
-        this.ports.add(port);
-    }
+	public void addPort(int port) {
+		if (this.ports == null) {
+			this.ports = new ArrayList<Integer>();
+		}
+		this.ports.add(port);
+	}
 
-    public Collection<Task> getTasks() {
-        return tasks;
-    }
+	public Collection<Task> getTasks() {
+		return tasks;
+	}
 
-    public void setTasks(Collection<Task> tasks) {
-        this.tasks = tasks;
-    }
+	public void setTasks(Collection<Task> tasks) {
+		this.tasks = tasks;
+	}
 
-    public Integer getTasksStaged() {
-        return tasksStaged;
-    }
+	public Integer getTasksStaged() {
+		return tasksStaged;
+	}
 
-    public void setTasksStaged(Integer tasksStaged) {
-        this.tasksStaged = tasksStaged;
-    }
+	public void setTasksStaged(Integer tasksStaged) {
+		this.tasksStaged = tasksStaged;
+	}
 
-    public Integer getTasksRunning() {
-        return tasksRunning;
-    }
+	public Integer getTasksRunning() {
+		return tasksRunning;
+	}
 
-    public void setTasksRunning(Integer tasksRunning) {
-        this.tasksRunning = tasksRunning;
-    }
+	public void setTasksRunning(Integer tasksRunning) {
+		this.tasksRunning = tasksRunning;
+	}
 
     public Collection<HealthCheck> getHealthChecks() {
         return healthChecks;
@@ -209,32 +209,32 @@ public class App {
         this.deployments.add(deployment);
     }
 
-    public List<String> getDependencies() {
-        return dependencies;
-    }
+	public List<String> getDependencies() {
+		return dependencies;
+	}
 
-    public void setDependencies(List<String> dependencies) {
-        this.dependencies = dependencies;
-    }
+	public void setDependencies(List<String> dependencies) {
+		this.dependencies = dependencies;
+	}
 
-    public Map<String, String> getLabels() {
-        return labels;
-    }
+	public Map<String, String> getLabels() {
+		return labels;
+	}
 
-    public void setLabels(Map<String, String> labels) {
-        this.labels = labels;
-    }
+	public void setLabels(Map<String, String> labels) {
+		this.labels = labels;
+	}
 
-    public void addLabel(String key, String value) {
-        if (this.labels == null) {
-            this.labels = new HashMap<String, String>();
-        }
-        this.labels.put(key, value);
-    }
+	public void addLabel(String key, String value) {
+		if (this.labels == null) {
+			this.labels = new HashMap<String, String>();
+		}
+		this.labels.put(key, value);
+	}
 
-    @Override
-    public String toString() {
-        return ModelUtils.toString(this);
-    }
+	@Override
+	public String toString() {
+		return ModelUtils.toString(this);
+	}
 
 }
